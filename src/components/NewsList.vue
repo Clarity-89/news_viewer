7c98f307dfd306b3c9de6c1c506c8fd3c46b7e38
Clarity-89 <homes89@ukr.net>
2017-03-28 22:29:41 +0300

    Fetch articles

--- src/components/NewsList.vue
+++ src/components/NewsList.vue
@@ -1,23 +1,33 @@
 <template>
-
+    <ul class="news-list">
+        <li v-for="(item, index) in news">
+            {{ item.author }} - {{ item.description }}
+        </li>
+    </ul>
 </template>
 
 <script>
-    import NewsItem from './NewsItem.vue';
+    import {config} from '../../config/local.env.js';
 
     export default {
         name: 'news-list',
-        components: NewsItem,
+
         data() {
             return {
                 news: [],
                 sources: []
             }
+        },
+
+        created() {
+            let self = this;
+            window.fetch(`https://newsapi.org/v1/articles?source=techcrunch&apiKey=${config.apiKey}`)
+                .then(response => response.json())
+                .then(response => {
+                    self.news = response.articles;
+                });
         }
     }
 </script>
 
-<style lang="scss">
-
-
-</style>
+<style></style>
