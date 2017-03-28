4bbd0504085b8f9121b9c00f776ad0919b758adb
Clarity-89 <homes89@ukr.net>
2017-03-28 16:07:06 +0300

    Add components

---
+++ src/components/NewsList.vue
@@ -0,0 +1,23 @@
+<template>
+
+</template>
+
+<script>
+    import NewsItem from './NewsItem.vue';
+
+    export default {
+        name: 'news-list',
+        components: NewsItem,
+        data() {
+            return {
+                news: [],
+                sources: []
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+
+
+</style>
