35a1247006225ca21f10a8d6b08ae74cf6455b83
Clarity-89 <homes89@ukr.net>
2017-03-29 15:51:54 +0300

    Add some styling

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,30 @@
 <template>
-  <div id="app">
-    <NewsList></NewsList>
-  </div>
+    <div id="app">
+        <NewsList></NewsList>
+    </div>
 </template>
 
 <script>
-import NewsList from './components/NewsList.vue'
+    import '../node_modules/material-design-lite/material.js';
+    import NewsList from './components/NewsList.vue'
 
-export default {
-  name: 'app',
-  components: {
-    NewsList
-  }
-}
+    export default {
+        name: 'app',
+        components: {
+            NewsList
+        }
+    }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+    @import "../node_modules/material-design-lite/material.min.css";
+
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
--- src/components/NewsList.vue
+++ src/components/NewsList.vue
@@ -1,13 +1,26 @@
 <template>
-    <ul class="news-list">
-        <li v-for="(article, index) in news">
-            <img :src="article.urlToImage"/>
+    <ul class="news-list mdl-grid">
+        <li v-for="(article, index) in news"
+            class="mdl-card mdl-card__custom mdl-shadow--2dp mdl-cell mdl-cell--2-col">
+            <div class="mdl-card__media cover" v-bind:class="tallClass(index)"
+                 v-bind:style="{'background-image': 'url('+ article.urlToImage +')'}"></div>
+
             {{article.publishedAt}} - {{ article.author }}
 
-            <a :href="article.url"><h2>{{article.title}}</h2></a>
-            <p>{{ article.description }}</p>
+            <a :href="article.url">
+            <h2 class="mdl-card__title-text">{{article.title}}</h2>
+        </a>
+            <div class="mdl-card__supporting-text">
+                <p>{{ article.description }}</p>
+            </div>
+            <div class="mdl-card__actions mdl-card--border">
+                <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+                    View Updates
+                  </a>
+            </div>
         </li>
     </ul>
+
 </template>
 
 <script>
@@ -23,6 +36,12 @@
             }
         },
 
+        methods: {
+            tallClass(index) {
+                return index % 2 === 0 ? 'tall' : '';
+            }
+        },
+
         created() {
             let self = this;
             window.fetch(`https://newsapi.org/v1/articles?source=techcrunch&apiKey=${config.apiKey}`)
@@ -35,4 +54,25 @@
     }
 </script>
 
-<style></style>
+<style lang="scss">
+
+    $img-height: 320px;
+
+    .news-list {
+        padding: 0 7em;
+        .mdl-card__custom {
+            margin-bottom: 1em;
+
+            .cover {
+                width: auto;
+                height: $img-height;
+                background-position: center;
+
+                &.tall {
+                    height: $img-height * 1.5;
+                }
+            }
+        }
+
+    }
+</style>
