f213d1d4a4443c2df83f711bf5f03808e232dc4c
Clarity-89 <homes89@ukr.net>
2017-03-30 15:58:37 +0300

    Masonry layout

--- src/components/NewsList.vue
+++ src/components/NewsList.vue
@@ -1,23 +1,25 @@
 <template>
-    <ul class="news-list mdl-grid">
+    <ul class="news-list">
         <li v-for="(article, index) in news"
-            class="mdl-card mdl-card__custom mdl-shadow--2dp mdl-cell mdl-cell--2-col">
-            <div class="mdl-card__media cover" v-bind:class="tallClass(index)"
-                 v-bind:style="{'background-image': 'url('+ article.urlToImage +')'}"></div>
+            class="mdl-card__wrapper">
+            <div class="mdl-card mdl-card__custom mdl-shadow--2dp">
+                <div class="mdl-card__media cover" v-bind:class="tallClass(index)"
+                     v-bind:style="{'background-image': 'url('+ article.urlToImage +')'}"></div>
 
-            {{article.publishedAt|time}} - {{ article.author }}
+                {{article.publishedAt | time}} - {{ article.author }}
 
             <a :href="article.url">
                 <h2 class="mdl-card__title-text">{{article.title}}</h2>
             </a>
 
-            <div class="mdl-card__supporting-text">
-                <p>{{ article.description }}</p>
-            </div>
-            <div class="mdl-card__actions mdl-card--border">
-                <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
-                    View Updates
-                  </a>
+                <div class="mdl-card__supporting-text">
+                    <p>{{ article.description }}</p>
+                </div>
+                <div class="mdl-card__actions mdl-card--border">
+                    <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+                        View Updates
+                      </a>
+                </div>
             </div>
         </li>
     </ul>
@@ -51,7 +53,7 @@
 
         created() {
             let self = this;
-            window.fetch(`https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=${config.apiKey}`)
+            window.fetch(`https://newsapi.org/v1/articles?source=ign&sortBy=top&apiKey=${config.apiKey}`)
                 .then(response => response.json())
                 .then(response => {
                     console.log('resp', response)
@@ -66,27 +68,35 @@
     $img-height: 320px;
 
     .news-list {
-        padding: 0 7em;
-        .mdl-card__custom {
-            margin-bottom: 1em;
-            padding-bottom: 50px;
+        padding: 10em;
 
-            .cover {
-                width: auto;
-                height: $img-height;
-                background-position: center;
 
-                &.tall {
-                    height: $img-height * 1.5;
+        column-count: 4;
+        column-gap: 0;
+
+        .mdl-card__wrapper {
+            break-inside: avoid-column;
+
+            .mdl-card__custom {
+                margin-bottom: 1em;
+                padding-bottom: 50px;
+
+                .cover {
+                    width: auto;
+                    height: $img-height;
+                    background-position: center;
+
+                    &.tall {
+                        height: $img-height * 1.5;
+                    }
+                }
+
+                .mdl-card__actions {
+                    position: absolute;
+                    bottom: 0;
+                    height: 50px;
                 }
             }
-
-            .mdl-card__actions {
-                position: absolute;
-                bottom: 0;
-                height: 50px;
-            }
         }
-
     }
 </style>
